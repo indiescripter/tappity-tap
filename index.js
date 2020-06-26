a4cf5190f0eb9313d3152020553f9fe471aa79ad
Justin Johansson <connect@indiescripter.com>
2020-06-26 20:03:37 +0930

    test: add typing tests

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
  * Copyright (c) 2020 Justin Johansson
  */
 
+'use strict';
+
 // @ts-expect-error
 const _default = require('tap');
 module.exports = _default;
---
+++ tests/index.js
@@ -0,0 +1,12 @@
+/*!
+ * Copyright (c) 2019 Justin Johansson
+ */
+
+'use strict';
+
+const tap = require('../index');
+const test = tap.test;
+
+test('Placeholder', (t) => {
+  t.end();
+});
